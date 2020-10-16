37b71876e54e855d3054620c50013dd4e5d901a8
ib-fhgr <ingmar.baetge@fhgr.ch>
2020-10-16 12:48:50 +0200

    kommentare

--- lib/http2/http2_app.dart
+++ lib/http2/http2_app.dart
@@ -31,7 +31,10 @@ class Http2App extends StatelessWidget {
               RaisedButton(
                 child: Text("POST"),
                 onPressed: () async {
-                  // http get Request
+                  // http post Request
+
+                  // für ios
+                  // var response = await http.post("http://127.0.0.1:5000/data",
                   var response = await http.post("http://10.0.2.2:5000/data",
                       headers: {
                         'Content-Type': 'application/json; charset=UTF-8'
